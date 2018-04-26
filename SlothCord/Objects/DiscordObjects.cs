53df37614683d802800fcd7c99bc3a09b8a47c21
Liam Holt <li223@users.noreply.github.com>
2018-04-26 15:39:02 +0100

    Update DiscordObjects.cs

--- SlothCord/Objects/DiscordObjects.cs
+++ SlothCord/Objects/DiscordObjects.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Drawing;
 
-namespace SlothCord
+namespace SlothCord.Objects
 {
     public sealed class DiscordUser : UserMethods
     {
