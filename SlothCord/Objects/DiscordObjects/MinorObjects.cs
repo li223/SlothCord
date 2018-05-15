d535cc460e2318422971320d7696e2ff12051867
Liam Holt <li223@users.noreply.github.com>
2018-05-15 10:23:03 +0100

    Update MinorObjects.cs

--- SlothCord/Objects/DiscordObjects/MinorObjects.cs
+++ SlothCord/Objects/DiscordObjects/MinorObjects.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.Linq;
 using System.Collections.Generic;
@@ -36,7 +36,32 @@ namespace SlothCord.Objects
         public string Url { get; set; }
     }
 
-    public sealed class DiscordApplication { }
+    public struct DiscordApplication
+    {
+        [JsonProperty("id")]
+        public ulong Id { get; private set; }
+
+        [JsonProperty("name")]
+        public string Name { get; private set; }
+
+        [JsonProperty("bot_public")]
+        public bool IsPublicBot { get; private set; }
+
+        [JsonProperty("bot_require_code_grant")]
+        public bool RequiresCodeGrant { get; private set; }
+
+        [JsonProperty("owner")]
+        public DiscordUser Owner { get; private set; }
+
+        [JsonProperty("description")]
+        public string Description { get; private set; }
+
+        [JsonProperty("icon")]
+        private string Icon { get; set; }
+
+        [JsonIgnore]
+        public string IconUrl { get => $"https://cdn.discordapp.com/icons/{this.Id}/{this.Icon}.png"; }
+    }
 
     public sealed class DiscordActivity
     {
