a8fd59c78e48e1d227be2f93fef3b57ac2b2863e
Liam Holt <27295070+li223@users.noreply.github.com>
2018-06-29 10:11:39 +0100

    Update and rename ApiClient.cs to ApiBase.cs

---
+++ SlothCord/SlothCord/Client/ApiBase.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SlothCord.Objects
+{
+    public static class Extensions
+    {
+        private static int _ratelimit { get; set; }
+        private static DispatchType _prevType { get; set; }
+        private static DateTimeOffset _lastRequestStamp { get; set; }
+
+        public static async Task<string> SendRequestAsync(this HttpClient http, HttpRequestMessage msg, DispatchType type)
+        {
+            string content = "";
+            switch (type)
+            {
+                case DispatchType.MessageCreate:
+                    {
+                        if (_prevType != DispatchType.MessageCreate)
+                        {
+                            _prevType = DispatchType.MessageCreate;
+                            _ratelimit = 5;
+                        }
+                        else _ratelimit -= 1;
+                        var timediff = DateTimeOffset.Now - _lastRequestStamp;
+                        if (timediff.Seconds > 5)
+                        {
+                            _lastRequestStamp = DateTime.Now;
+                            _ratelimit = 5;
+                        }
+                        if (_ratelimit <= -1 && timediff.Seconds <= 5)
+                            content = await InternalWaitAsync(http, 5000, msg).ConfigureAwait(false);
+                        else
+                        {
+                            var response = await http.SendAsync(msg).ConfigureAwait(false);
+                            content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        }
+                        break;
+                    }
+
+                case DispatchType.MessageDelete:
+                    {
+                        if (_prevType != DispatchType.MessageDelete)
+                        {
+                            _prevType = DispatchType.MessageDelete;
+                            _ratelimit = 5;
+                        }
+                        else _ratelimit -= 1;
+                        var timediff = DateTimeOffset.Now - _lastRequestStamp;
+                        if (timediff.Seconds > 1)
+                        {
+                            _lastRequestStamp = DateTime.Now;
+                            _ratelimit = 5;
+                        }
+                        if (_ratelimit <= -1 && timediff.Seconds <= 1)
+                            content = await InternalWaitAsync(http, 1000, msg).ConfigureAwait(false);
+                        else
+                        {
+                            var response = await http.SendAsync(msg).ConfigureAwait(false);
+                            content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        }
+                        break;
+                    }
+            }
+            return content;
+        }
+
+        static async Task<string> InternalWaitAsync(HttpClient client, int retry_in, HttpRequestMessage msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[{DateTime.Now.ToShortTimeString()}] ->  Internal Ratelimit Reached, waiting {retry_in}ms");
+            Console.ForegroundColor = ConsoleColor.White;
+            await Task.Delay(retry_in).ConfigureAwait(false);
+            var response = await client.SendAsync(msg).ConfigureAwait(false);
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return content;
+        }
+    }
+}
