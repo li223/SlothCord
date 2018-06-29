bc9f01193be20ca67c942cf6e2fc93a86df84fbb
Liam Holt <27295070+li223@users.noreply.github.com>
2018-06-29 09:59:20 +0100

    Update Events.cs

--- SlothCord/SlothCord/Client/Events.cs
+++ SlothCord/SlothCord/Client/Events.cs
@@ -1,7 +1,5 @@
-﻿using SlothCord.Objects;
-using System;
+using SlothCord.Objects;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SlothCord
@@ -11,4 +9,7 @@ namespace SlothCord
     public delegate Task Heartbeated(object sender);
     public delegate Task GuildsDownloaded(object sender, IEnumerable<DiscordGuild> Guilds);
     public delegate Task GuildCreated(object sender, DiscordGuild guild);
+    public delegate Task UnkownOpCode(object sender, int code, string name);
+    public delegate Task UnkownEvent(object sender, int code, string payload);
+    public delegate Task MessageCreated(object sender, DiscordMessage message);
 }
